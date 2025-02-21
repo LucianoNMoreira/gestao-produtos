0640ae62d7da50e144d80b2970a8f3e42724b2c3
Luciano Nobre Moreira <lucianonobremoreira@gmail.com>
2025-02-20 21:25:59 -0300

    armazenar usuario no bd

--- src/app/api/v1/auth/login/route.ts
+++ src/app/api/v1/auth/login/route.ts
@@ -1,19 +1,26 @@
-
 import jwt from 'jsonwebtoken'
+import Usuario from '@/db/models/usuario'
+const bcrypt = require('bcrypt')
 
 export async function POST(req: any) {
     try {
         const { login, senha } = await req.json()
 
-        if (login != 'admin' || senha != 'admin') {
-            throw 'Ops'
+        const salt = await bcrypt.genSalt(10)
+        const senhaCripto = await bcrypt.hash(senha, salt)
+
+        const user = await Usuario.findOne({ login: login })
+
+        if (!user || !(await bcrypt.compare(senha, user.senha))) {
+            throw 'Credenciais inválidas'
         }
 
         return Response.json({
             token: jwt.sign(
                             {
                                 user: {
-                                    id: '123'
+                                    id: user._id,
+                                    login: user.login
                                 }
                             }, 
                             process.env.JWT_SECRET!,
@@ -22,5 +29,10 @@ export async function POST(req: any) {
         })
     } catch (error: any) {
         console.log("Erro", error)
+        return Response.json({
+                error: error
+            },
+            { status: 401 }
+        )
     }
 }
---
+++ src/db/models/usuario.ts
@@ -0,0 +1,14 @@
+import mongoose from '@/db/mongodb'
+
+const UsuarioSchema = new mongoose.Schema({
+    login: { type: String, required: true, index: true },
+    senha: { type: String, required: true }
+  },
+  {
+    versionKey: false
+  }
+)
+
+const Usuario = mongoose.models.Usuario || mongoose.model('Usuario', UsuarioSchema)
+
+export default Usuario
