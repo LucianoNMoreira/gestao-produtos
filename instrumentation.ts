f5e3c6af9dab88452562d8ddd310eee3e7483992
Luciano Nobre Moreira <lucianonobremoreira@gmail.com>
2025-03-06 14:13:09 -0300

    criar usuário admin ao executar o projeto

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,3 +1,18 @@
-// import "@/db/mongodb"
+import Usuario from '@/db/models/usuario'
+import bcrypt from 'bcrypt'
 
 console.log("iniciando instrumentations.ts")
+
+Usuario.findOne({
+    login: 'admin'
+}).then((user) => {
+    if(!user) {
+        console.log("Criando usuário admin|admin")
+        bcrypt.hash('admin', 10, function(err, hash) {
+            Usuario.create({
+                login: 'admin',
+                senha: hash
+            })
+        });
+    }
+})
