61c620ce279edb24874973d539b57cc81d331fd1
Luciano Nobre Moreira <lucianonobremoreira@gmail.com>
2025-03-05 22:05:49 -0300

    renomear arquivo .msw

--- src/test/msw_server.ts
+++ src/test/msw_server.ts
@@ -1,5 +1,5 @@
 import { setupServer } from 'msw/node'
-import produtos_handlers from '../app/api/v1/produtos/produtos.msw'
+import produtos_handlers from '../app/api/v1/produtos/route.msw'
 
 export const server = setupServer(
 	...produtos_handlers
